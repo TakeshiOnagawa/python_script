4abe4b82377e6f6be8663fb3b84c375973a9f501
Takeshi Onagawa <gsmcustomeffects@gmail.com>
2025-01-04 04:18:35 +0900

    Update pyvisa01.py
    プログラム中にコメントを追加

--- pyvisa01.py
+++ pyvisa01.py
@@ -8,28 +8,34 @@ rm = pyvisa.ResourceManager()
 visa_list = rm.list_resources()
 print(visa_list)
 
-#ここはprint結果によって各自書き換える
+#Hantek HDM3065のSCPIをオープンする(ここはprint結果によって各自書き換える)
 hdm3065 = rm.open_resource('USB0::0x049F::0x606E::CN2303013001068::INSTR')
 
 #IDN?は機器情報を取得するコマンド
 hdm3065.write('*IDN?')
 
-#タイムアウト指定
+#タイムアウト指定[ms]
+#これがないとREAD命令で
 hdm3065.timeout = 25000
 
-#init
-hdm3065.write('*RST')
-time.sleep(1)
-hdm3065.write('*CLS')
-time.sleep(1)
+#HDM3065のInit
+hdm3065.write('*RST')                   #キューのRESET命令
+time.sleep(3)
+hdm3065.write('*CLS')                   #キューのCLEAR命令
+time.sleep(0.3)
+hdm3065.write('CONF:VOLT:AC 100 mV')    #機器のCONF命令
+time.sleep(3)
+hdm3065.write('SAMP:COUN 1')            #サンプル数指定
+time.sleep(0.3)
 
+#デジタルマルチメータから連続でREADする
 values = []
-for i in range(208):
-    hdm3065.write('CONF:VOLT:AC 100 mV')
+for i in range(10):
     currentValue = np.array(hdm3065.query_ascii_values('READ?'))
     print(str(i)+':'+str(currentValue))
     values.append(currentValue)
 
+#取得後の統計値を出力
 print('max value is : ' + str(np.max(values)))
 print('min value is : ' + str(np.min(values)))
 print('average value is : ' + str(np.average(values)))
